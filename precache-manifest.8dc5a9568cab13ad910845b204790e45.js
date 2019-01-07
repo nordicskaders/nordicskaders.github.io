db2aac6694141a57eb0ec902d62a4b900592ebb7
andersbando-hess <bandersman@gmail.com>
2019-01-07 10:49:37 -0500

    Updates

---
+++ precache-manifest.8dc5a9568cab13ad910845b204790e45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0999145c2e70ca57824acd5bea616625",
+    "url": "/static/media/two.0999145c.jpg"
+  },
+  {
+    "revision": "1dcdd93037f2704bcdd878e97688eed9",
+    "url": "/static/media/three.1dcdd930.jpg"
+  },
+  {
+    "revision": "40d8704ec9aaa24274daf3ab7de71b6c",
+    "url": "/static/media/one.40d8704e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3bd821d487201534f9a9",
+    "url": "/static/js/main.3bd821d4.chunk.js"
+  },
+  {
+    "revision": "701d0f0b10bc220c3ef5",
+    "url": "/static/js/1.701d0f0b.chunk.js"
+  },
+  {
+    "revision": "3bd821d487201534f9a9",
+    "url": "/static/css/main.589f5d89.chunk.css"
+  },
+  {
+    "revision": "701d0f0b10bc220c3ef5",
+    "url": "/static/css/1.048b9bc4.chunk.css"
+  },
+  {
+    "revision": "fcfc0da7543d33ebe45c9e8e81fa7711",
+    "url": "/index.html"
+  }
+];
